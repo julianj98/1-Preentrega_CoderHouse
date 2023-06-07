63ef72a0b2cb1a95f86e71673a0e08e9b5b41161
Julian Juarez <julianjuarez98.jj@gmail.com>
2023-06-07 12:20:58 -0300

    se documento las consultas a los endpoints

--- src/app.js
+++ src/app.js
@@ -11,3 +11,20 @@ app.use('/api/carts',carts);
 app.listen(8080, () => {
     console.log("Server is running on port 8080");
 })
+/*CONSULTAS AL ENDPOINT (reemplazar la x por el id)
+para PRODUCTS
+GET
+localhost:8080/api/products
+localhost:8080/api/products/x
+POST
+localhost:8080/api/products/
+PUT Y DELETE
+localhost:8080/api/products/x
+
+para CARTS
+POST
+localhost:8080/api/carts
+GET
+localhost:8080/api/carts/X
+POST de un product en un cart
+localhost:8080/api/carts/x/product/x  */
